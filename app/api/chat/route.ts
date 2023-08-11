7903ed3b4da8ae88d6b2162422694300cb91e90e
moonhuanchen <141982949+moonhuanchen@users.noreply.github.com>
2023-08-11 11:42:57 +0800

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
     'content': 你扮演科學領域的中英文譯者
   }, ...histories.slice(-3, -1), {
     'role': 'user',
-    'content': `"""${question}""" 用專業的口吻，科學期刊內常用的單字呈現`
+    'content': `"""${question}""" 用專業的口吻，科學期刊內常用的單字呈現。`
   }]
 
   const res = await openai.createChatCompletion({
